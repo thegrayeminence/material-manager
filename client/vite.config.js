d655386faab0369eb9eb3b1c7d83d4644a80f3ed
Oliver Gray <olliewgray@gmail.com>
2024-01-28 23:56:16 -0500

    prepping folder reorg

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -6,5 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+
   }
 })
