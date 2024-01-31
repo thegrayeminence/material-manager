55c2f5e952a5f9c0c9ef240a00e2a047a419df84
Oliver Gray <olliewgray@gmail.com>
2024-01-31 12:02:23 -0500

    some styling tweaks to landing page....again

--- client/src/pages/LandingPage/components/LandingPageText.jsx
+++ client/src/pages/LandingPage/components/LandingPageText.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import {useNavigate} from 'react-router-dom';
 
 import {motion} from 'framer-motion';
-import {Stack, VStack, HStack, Box, Button, Text, Spacer} from '@chakra-ui/react';
+import {Stack, Center, VStack, HStack, Box, Button, Text, Spacer, chakra, Divider} from '@chakra-ui/react';
 import {StylishHeader, MotionContainer, StylishButton} from '../../../components';
 
 //fonts
@@ -16,24 +16,35 @@ import '@fontsource/inter';
 
 function LandingPageText() {
 
-    const HeaderStyle = {
+    const H1 = {
         bgGradient: "linear(to-l, #7928CA, #FF0080)",
         bgClip: "text",
-        letterSpacing: '.35rem',
-        fontSize: '6rem',
+        letterSpacing: '.15rem',
+        fontSize: '7rem',
         textAlign: 'center',
-        // textShadow: "1px 1px 1px rgba(0, 0, 0, 0.25)",
-        fontFamily: "inter, sans-serif",
+        //textShadow: "1px 1px 1px rgba(0, 0, 0, 0.25)",
+        fontFamily: "poppins, sans-serif",
+        fontWeight: '900',
+    };
+
+    const H2 = {
+        bgGradient: "linear(to-r, teal.400, blue.400)",
+        bgClip: "text",
+        letterSpacing: '.15rem',
+        fontSize: '7rem',
+        textAlign: 'center',
+        //textShadow: "1px 1px 1px rgba(0, 0, 0, 0.25)",
+        fontFamily: "poppins, sans-serif",
         fontWeight: '900',
     };
 
     const detailHeaderStyle = {
-        bgGradient: "linear(to-r, #9bf8f4, #6f7bf7)",
-        bgClip: "text",
-        letterSpacing: '.15rem',
-        fontSize: '2.15rem',
+        //  bgGradient: "linear(to-r, #9bf8f4, #6f7bf7)",
+        color: 'white.800',
+        // bgClip: "text",
+        // letterSpacing: '.15rem',
+        fontSize: '1.9rem',
         textAlign: 'center',
-        //textShadow: "2px 2px 2px rgba(0, 0, 0, 0.1)",
         fontFamily: "poppins, sans-serif",
         fontWeight: '600',
 
@@ -49,38 +60,50 @@ function LandingPageText() {
 
     return (
 
+        <Box pos="relative" overflow="hidden">
+            <Box maxW="7xl" mx="auto">
+                <Box pos="relative" w="full" border="solid 1px transparent" >
+                    <Box maxW={{base: "7xl", }} mt={'20%'} >
 
+                        <Box textAlign="center" mx="auto" w={'90%'} >
+                            <StylishHeader text={"TEXTURE \n FORGE"} sx={H1} />
+                            {/* <StylishHeader text={"FORGE"} sx={H2} /> */}
+                            {/* <Center>
+                                <Divider textAlign={'center'} borderWidth={'.25rem'} width={'full'} color='white.800' borderStyle={'solid'} />
+                            </Center> */}
+                            <Center>
+                                <Text py={'2rem'} sx={detailHeaderStyle} maxW={'xl'} >
+                                    Generate, Preview, and Download PBR Textures for 3D Materials
+                                </Text>
+                            </Center>
+                        </Box>
+                        <Center>
+                            <Divider textAlign={'center'} borderWidth={'.25rem'} maxW={'full'} color='white.800' borderStyle={'solid'} />
+                        </Center>
 
+                        <Stack justifyContent="center" py={'2.5rem'} >
+                            <HStack justifyContent={'center'}>
+                                <StylishButton mr='.5rem' handleClick={() => navigate('/preview')} text="Generate Maps" />
+                                <StylishButton ml='.5rem' handleClick={() => navigate('/preview')} text="Download Maps" />
+                            </HStack>
+                            <Box alignSelf={'center'} mt='5'>
+                                <Button colorScheme='white' variant='ghost'>How does it work? What is PBR? </Button>
+                            </Box>
 
-        <Box width='100vw' h='100vh'>
-            <MotionBox
-                position="relative"
-                h='100%'
-                ml='10%'
-                w="80%"
-                display="flex"
-                justifyContent="center"
-                flexDirection="column"
-                initial='initial'
-                animate='animate'
-            >
-
-                <Box >
-                    <StylishHeader text="TEXTURE FORGE" sx={HeaderStyle} />
-                    <Spacer py={'.25rem'} />
-                    <StylishHeader text="GENERATE/DOWNLOAD/PREVIEW TEXTURE MAPS FOR PBR MATERIALS" sx={detailHeaderStyle} />
-                    <Spacer py={'2.5rem'} />
+                        </Stack>
+                    </Box>
                 </Box>
-                <HStack justifyContent={'center'}>
-                    <StylishButton mr='.5rem' handleClick={() => navigate('/preview')} text="Generate Maps" />
-                    <StylishButton ml='.5rem' handleClick={() => navigate('/preview')} text="Download Maps" />
-                </HStack>
-                <Box alignSelf={'center'} mt='5'>
-                    <Button variant='ghost'>Questions?</Button>
-                </Box>
-            </MotionBox>
+            </Box>
+        </Box >
 
-        </Box>
+
+
+
+
+
+
+
+
 
     )
 }
