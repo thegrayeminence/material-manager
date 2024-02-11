1b3e207f01c4e7e782785678e23e1fefa37b9530
Oliver Gray <olliewgray@gmail.com>
2024-02-10 23:07:16 -0500

    ...

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig(({mode}) => {
     plugins: [react()],
     server: {
 
-      port: 3000,
+      port: process.env.PORT || 3000,
       ...(isDevelopment && {
         proxy: {
           '/api': {
