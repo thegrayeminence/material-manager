bd16e83262ba4688121cdf9592d75853b49e5270
Oliver Gray <olliewgray@gmail.com>
2024-02-17 22:10:54 -0500

    working on endpoint fixes still

--- server/config.py
+++ server/config.py
@@ -626,10 +626,10 @@ def get_recent_albedo():
 
 
     
-# if __name__ == '__main__':
-#     app.run(host="0.0.0.0", port=10000)
-#     # port = int(os.environ.get('PORT', 10000))
-#     # app.run(host='0.0.0.0', port=port, debug=False)
+if __name__ == '__main__':
+    # app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
 
     
     
